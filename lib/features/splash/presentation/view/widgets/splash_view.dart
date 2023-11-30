9e097f1e4a9fdb77874919628c1cdd9cfa4b86d3
Fyala <www.ahmedfyala621@gmail.com>
2023-11-30 04:12:39 +0200

    Initial commit,add splash view

---
+++ lib/features/splash/presentation/view/widgets/splash_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SolashViewBody(),
+    );
+  }
+}
+class SolashViewBody extends StatelessWidget {
+  const SolashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
